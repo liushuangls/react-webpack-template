167dd20e989d4e88e6f48c5e56207958091caf8d
liushuang <liushuangbill@outlook.com>
2018-05-07 21:20:02 +0800

    使用gzip，减少打包文件大小

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require("webpack");
 const HTMLPlugin = require("html-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CompressionPlugin = require("compression-webpack-plugin");
 
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
@@ -56,11 +57,28 @@ module.exports = {
     new ExtractTextPlugin("main.css"),
     // 模块热替换插件
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    // 提供带 Content-Encoding 编码的压缩版的资源
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: new RegExp("."),
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    // 减小文件大小
+    new webpack.DefinePlugin({
+      // <-- 减少 React 大小的关键
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin() //合并块
   ],
   devServer: {
     port: 8080,
     contentBase: resolve("dist"),
+    compress: true,
     hot: true,
     inline: true,
     host: "0.0.0.0",
